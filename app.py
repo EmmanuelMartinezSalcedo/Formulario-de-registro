ebb89e9079b6d557077d929ad919a6a2cf0a1742
EmmanuelMartinezSalcedo <emmanuel.martinez@ucsp.edu.pe>
2022-05-21 16:40:28 -0500

    casi final

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateField, SelectField, FileField
+from wtforms import StringField, PasswordField, SubmitField, DateField, SelectField, FileField, IntegerField
 from wtforms.validators import InputRequired, Length, ValidationError
 import os
 from werkzeug.utils import secure_filename
@@ -11,19 +12,15 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_BINDS']={'images': 'sqlite:///images.db'}
 app.config['SECRET_KEY'] = 'secretkey'
 db = SQLAlchemy(app)
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-def validate_file_extension(value):
-  import os
-  ext = os.path.splitext(value.name)[1]
-  valid_extensions = ['.png','.jpg']
-  if not ext in valid_extensions:
-    raise ValidationError(u'Solo soporta imagenes')
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -35,6 +32,12 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
+class images(db.Model):
+    __bind_key__ ='images'
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
 
 class RegisterForm(FlaskForm):
     name = StringField(validators=[
@@ -42,8 +45,7 @@ class RegisterForm(FlaskForm):
     lastname = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Apellido"})
 
-    birthday = DateField('Start Date', format='%d/%m/%y', validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Fecha de nacimiento"})
+    birthday = DateField('birthday', format='%Y-%m-%d', validators=[InputRequired()],render_kw={"placeholder": "Fecha de nacimiento"})
     username = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Correo"})
 
@@ -64,19 +66,19 @@ class RegisterForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Correo"})
 
     password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Contraseña"})
 
     submit = SubmitField('Iniciar sesión')
 
 class ProductForm(FlaskForm):
     name = StringField(validators=[
                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Nombre"})
-    price = StringField(validators=[
-                           InputRequired(), Length(min=1, max=20)], render_kw={"placeholder": "Precio"})
-    image = FileField("File", validators=[InputRequired(),validate_file_extension])
+    price = IntegerField(validators=[
+                           InputRequired()], render_kw={"placeholder": "Precio"})
+    image = FileField("File", validators=[InputRequired()])
 
     submit = SubmitField('Listo')
 
@@ -93,14 +95,14 @@ def login():
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('tienda'))
     return render_template('login.html', form=form)
 
 
-@app.route('/dashboard', methods=['GET', 'POST'])
+@app.route('/tienda', methods=['GET', 'POST'])
 @login_required
-def dashboard():
-    return render_template('dashboard.html')
+def tienda():
+    return render_template('tienda.html')
 
 
 @app.route('/logout', methods=['GET', 'POST'])
@@ -116,21 +118,19 @@ def register():
 
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password,
-        name=form.name.data, lastname=form.lastname.data, birthday=form.birthday.data)
+        new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
-
     return render_template('register.html', form=form)
 
 @ app.route('/product', methods=['GET', 'POST'])
 def product():
     form = ProductForm()
-
     if form.validate_on_submit():
-        file = form.file.data
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
-        return redirect(url_for('dashboard'))
-
+        new_product = images(name=form.name.data, img=form.image.data, price=form.price.data)
+        db.session.add(new_product)
+        db.session.commit()
+        return redirect(url_for('tienda'))
     return render_template('product.html', form=form)
+    
